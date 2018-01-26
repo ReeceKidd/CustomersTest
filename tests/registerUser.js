4322518265588d0e460eb06cc2fad68d7855eed8
Reece Kidd <reecekidd95@gmail.com>
2018-01-26 23:52:56 +0000

    Added latitude and longitude minimum and maximum tests

--- tests/registerUser.js
+++ tests/registerUser.js
@@ -14,6 +14,16 @@ chai.use(chaiHttp);
 //Empty the test database before starting
 Customer.remove({}, function(err) { 
  })
+
+/*
+Tests: 
+// Test for valid registration
+// Test for Customers with the same Customer_id being passed to the database
+// Test for additional fields being passed to request
+// Tests that Customer_id is a number
+// Tests that the minimum length of name is two characters
+// Tests that name is not a blank string
+*/
  
 describe('Test for valid registration', () => {
     
@@ -126,6 +136,74 @@ describe('Tests that name is not a blank string', () => {
             .send({
                 latitude: "52.986375",
                 longitude: "-6.043701",
+                Customer_id: 7,
+                name: ""
+            })
+            .end((err, res) => {
+                res.should.have.status(600)
+                done()
+            })
+    })
+})
+
+describe('Tests that latitiude cannot excede 90', () => {
+    it('It should fail as latitude is greater than 90', (done) => {
+        chai.request(server)
+            .post('/register-Customer')
+            .send({
+                latitude: "100.986375",
+                longitude: "-6.043701",
+                Customer_id: 7,
+                name: ""
+            })
+            .end((err, res) => {
+                res.should.have.status(600)
+                done()
+            })
+    })
+})
+
+describe('Tests that latitiude is not less than -90', () => {
+    it('It should fail as latitude is less than -90', (done) => {
+        chai.request(server)
+            .post('/register-Customer')
+            .send({
+                latitude: "-100.986375",
+                longitude: "-6.043701",
+                Customer_id: 7,
+                name: ""
+            })
+            .end((err, res) => {
+                res.should.have.status(600)
+                done()
+            })
+    })
+})
+
+describe('Tests that longitude is not greater than 180', () => {
+    it('It should fail as longitude is greater than 180', (done) => {
+        chai.request(server)
+            .post('/register-Customer')
+            .send({
+                latitude: "-10.986375",
+                longitude: "200.043701",
+                Customer_id: 7,
+                name: ""
+            })
+            .end((err, res) => {
+                res.should.have.status(600)
+                done()
+            })
+    })
+})
+
+describe('Tests that longitude is not less than -180', () => {
+    it('It should fail as longitude is less than -180', (done) => {
+        chai.request(server)
+            .post('/register-Customer')
+            .send({
+                latitude: "-54.986375",
+                longitude: "-190.043701",
                 Customer_id: 7,
                 name: ""
             })
